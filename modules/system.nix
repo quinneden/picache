4061127565b630e86b645aa7555e8b2695099a37
quinneden <quinnyxboy@gmail.com>
2024-12-27 15:55:51 -0800

    Modified: modules/system.nix Deleted: modules/hardware.nix

--- modules/system.nix
+++ modules/system.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 {
   fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
-      fsType = "ext4";
-      options = [ "noatime" ];
-    };
-
     "/nix" = {
       device = "/dev/disk/by-label/nix";
       fsType = "ext4";
